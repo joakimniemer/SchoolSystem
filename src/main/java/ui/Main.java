1855b4f6d1e2bd17b83a3bf4d8136b8e00a0e2ed
MRosTobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2023-02-13 14:55:39 +0100

    added to Main

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,13 +1,18 @@
 package ui;
 
 import models.Student;
+import models.Teacher;
 
 public class Main {
     public static void main(String[] args) {
 
         Student s1 = new Student("Joakim", 1992, 9, 14);
 
+        Teacher t1 = new Teacher("Hampus Gam", 52, "Balettinstruktör", 20000);
+
         System.out.println(s1);
+
+        System.out.println(t1);
 
     }
 }
